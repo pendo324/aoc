19b275e386c613819b34a0146af3026be444fc96
pendo324 <pendo3247@gmail.com>
2023-12-07 18:31:34 -0500

    add day 3 part 2 and refactor day 3 part 1

--- years/2023/03/main.go
+++ years/2023/03/main.go
@@ -2,10 +2,12 @@ package day
 
 import (
 	"fmt"
+	"image"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"github.com/pendo324/aoc/cmd"
 	"github.com/spf13/cobra"
@@ -17,103 +19,89 @@ func day(cmd *cobra.Command, _ []string) error {
 		return fmt.Errorf("failed to open input: %w", err)
 	}
 
-	if err := part1(string(in)); err != nil {
-		return fmt.Errorf("failed to run part1: %w", err)
-	}
-	if err := part2(string(in)); err != nil {
-		return fmt.Errorf("failed to run part2: %w", err)
-	}
+	rows := strings.Split(string(in), "\n")
+	part1(rows)
+	part2(rows)
 
 	return nil
 }
 
-func isDigit(r rune) bool {
-	return r >= '0' && r <= '9'
-}
+func setup(rows []string, f func(rune) bool) map[image.Point][]int {
+	partNumbers := map[image.Point]rune{}
+	adj := map[image.Point][]int{}
+	rowLen := 0
 
-func intPointer(b byte) *int {
-	i := int(b)
-	return &i
-}
-
-func part1(in string) error {
-	rows := strings.Split(in, "\n")
-	partNumbers := make([][]*int, len(rows))
 	for i := 0; i < len(rows); i++ {
 		row := rows[i]
-		if i == 0 {
-			partNumbers[i] = make([]*int, len(row))
-		}
-		if i < len(rows)-1 {
-			partNumbers[i+1] = make([]*int, len(row))
-		}
 		for j, col := range row {
-			if col != '.' && !isDigit(col) {
-				// left
-				if j > 0 && isDigit(rune(row[j-1])) {
-					partNumbers[i][j-1] = intPointer(row[j-1] - '0')
-				}
-				// right
-				if j < len(row)-1 && isDigit(rune(row[j+1])) {
-					partNumbers[i][j+1] = intPointer(row[j+1] - '0')
-				}
-				// above
-				if isDigit(rune(rows[i-1][j])) {
-					partNumbers[i-1][j] = intPointer(rows[i-1][j] - '0')
-				}
-				// top left
-				if j > 0 && isDigit(rune(rows[i-1][j-1])) {
-					partNumbers[i-1][j-1] = intPointer(rows[i-1][j-1] - '0')
-				}
-				// top right
-				if j < len(row)-1 && isDigit(rune(rows[i-1][j+1])) {
-					partNumbers[i-1][j+1] = intPointer(rows[i-1][j+1] - '0')
-				}
-				if i < len(row)-1 {
-					// below
-					if isDigit(rune(rows[i+1][j])) {
-						partNumbers[i+1][j] = intPointer(rows[i+1][j] - '0')
+			rowLen = len(row)
+			partNumbers[image.Pt(i, j)] = col
+		}
+	}
+
+	for i := 0; i < len(rows); i++ {
+		for j := 0; j < rowLen; j++ {
+			p := image.Pt(i, j)
+			n := partNumbers[p]
+			if unicode.IsDigit(n) {
+				numAcc := string(n)
+				numPoints := []image.Point{image.Pt(i, j)}
+				ps := map[image.Point]bool{}
+
+				for k := j + 1; ; k++ {
+					pp := p.Add(image.Pt(0, k-j))
+					nn := partNumbers[pp]
+					if unicode.IsDigit(nn) {
+						numAcc += string(nn)
+						numPoints = append(numPoints, image.Pt(i, k))
+					} else {
+						j = k
+						break
 					}
-					// bottom left
-					if j > 0 && isDigit(rune(rows[i+1][j-1])) {
-						partNumbers[i+1][j-1] = intPointer(rows[i+1][j-1] - '0')
+				}
+
+				num, _ := strconv.Atoi(numAcc)
+
+				adjacentPoints := []image.Point{
+					{-1, -1},
+					{-1, 0},
+					{-1, 1},
+					{0, -1},
+					{0, 1},
+					{1, -1},
+					{1, 0},
+					{1, 1},
+				}
+
+				for _, k := range numPoints {
+					for _, p := range adjacentPoints {
+						newPoint := k.Add(p)
+						if f(partNumbers[newPoint]) {
+							ps[newPoint] = true
+						}
 					}
-					// bottom right
-					if j < len(row)-1 && isDigit(rune(rows[i+1][j+1])) {
-						partNumbers[i+1][j+1] = intPointer(rows[i+1][j+1] - '0')
-					}
+				}
+
+				for p := range ps {
+					adj[p] = append(adj[p], num)
 				}
 			}
 		}
 	}
 
-	for i, row := range partNumbers {
-		for j := range row {
-			if row[j] != nil {
-				if j < len(row)-1 {
-					expand(rows[i], partNumbers, i, j+1)
-				}
-				if j > 0 {
-					expand(rows[i], partNumbers, i, j-1)
-				}
-			}
-		}
-	}
+	return adj
+}
+
+func part1(rows []string) error {
+	adj := setup(rows, func(r rune) bool {
+		return r != '.' && !unicode.IsDigit(r) && r != 0
+	})
 
 	acc := 0
-	for _, row := range partNumbers {
-		s := ""
-		for _, col := range row {
-			if col == nil {
-				n, _ := strconv.Atoi(s)
-				acc += n
-				s = ""
-			} else {
-				s += strconv.Itoa(*col)
-			}
+	for _, nums := range adj {
+		for _, n := range nums {
+			acc += n
 		}
-		n, _ := strconv.Atoi(s)
-		acc += n
 	}
 
 	fmt.Printf("Part One: %d\n", acc)
@@ -121,27 +109,19 @@ func part1(in string) error {
 	return nil
 }
 
-func expand(row string, arr [][]*int, i, j int) {
-	if j > 0 {
-		if arr[i][j] == nil {
-			if isDigit(rune(row[j])) {
-				arr[i][j] = intPointer(row[j] - '0')
-				expand(row, arr, i, j-1)
-			}
-		}
-	}
-	if j < len(arr[i])-1 {
-		if arr[i][j] == nil {
-			if isDigit(rune(row[j])) {
-				arr[i][j] = intPointer(row[j] - '0')
-				expand(row, arr, i, j+1)
-			}
-		}
-	}
-}
+func part2(rows []string) error {
+	adj := setup(rows, func(r rune) bool {
+		return r == '*'
+	})
 
-func part2(in string) error {
-	fmt.Printf("Part Two: %d\n", 0)
+	acc := 0
+	for _, nums := range adj {
+		if len(nums) == 2 {
+			acc += nums[0] * nums[1]
+		}
+	}
+
+	fmt.Printf("Part Two: %d\n", acc)
 
 	return nil
 }
